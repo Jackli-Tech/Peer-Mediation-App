6c0ef9f97f5c4fa3f6979492bdccdb4d98705462
dioStaxis <64261167+dioStaxis@users.noreply.github.com>
2021-06-07 19:03:15 +1200

    cleaned timetable.dart

--- lib/Pages/timetable.dart
+++ lib/Pages/timetable.dart
@@ -2,8 +2,6 @@ import 'package:Flutter_app/Model/timeModel.dart';
 import 'package:flutter/material.dart';
 import '../NetworkHandler.dart';
 import '../Model/TableModel.dart';
-import 'showtimetable.dart';
-import 'teacherhome.dart';
 
 class Timetable extends StatefulWidget {
   Timetable({Key key}) : super(key: key);
@@ -12,20 +10,13 @@ class Timetable extends StatefulWidget {
   _TimetableState createState() => _TimetableState();
 }
 
+//Init Variables
 GlobalKey<FormState> _mondayKey = GlobalKey<FormState>();
 GlobalKey<FormState> _tuesdayKey = GlobalKey<FormState>();
 GlobalKey<FormState> _wednesdayKey = GlobalKey<FormState>();
 GlobalKey<FormState> _thursdayKey = GlobalKey<FormState>();
 GlobalKey<FormState> _fridayKey = GlobalKey<FormState>();
 
-TimeModel localItem;
-NetworkHandler networkHandler = new NetworkHandler();
-var mondayNames = [];
-var tuesdayNames = [];
-var wednesdayNames = [];
-var thursdayNames = [];
-var fridayNames = [];
-int i = 0;
 List<String> newString;
 List<String> mondayString;
 List<String> tuesdayString;
@@ -33,59 +24,74 @@ List<String> wednesdayString;
 List<String> thursdayString;
 List<String> fridayString;
 
+var mondayNames = [];
+var tuesdayNames = [];
+var wednesdayNames = [];
+var thursdayNames = [];
+var fridayNames = [];
+
+int counter = 0;
+TimeModel localItem;
+NetworkHandler networkHandler = new NetworkHandler();
+
+/* getValue Function that returns the value of a specific day's string at 'counter' position in the day's String List. 
+** The String 'day' variable passed into the function determines which 'day'String the values are retrieved from.
+** 'counter' determines which TextFormField the value will be sent to, and which position from the String List it should be retrieved from.
+** As there are 18 fields and the index starts at 0, when the 'counter' reaches 17, the counter is set to 0, and the 17th element returned.
+** Returns "" in case of null/empty to provide a blank TextFormField, indicating no prior name saved in the database.
+*/
 getValue(String day) {
-  //print("getvalue");
   if (day == "Monday") {
     if ((mondayString != null) && (mondayString.isNotEmpty)) {
-      if (i == 17) {
-        i = 0;
+      if (counter == 17) {
+        counter = 0;
         return mondayString.elementAt(17);
       } else {
-        return mondayString.elementAt(i++);
+        return mondayString.elementAt(counter++);
       }
     } else {
       return "";
     }
   } else if (day == "Tuesday") {
     if ((tuesdayString != null) && (tuesdayString.isNotEmpty)) {
-      if (i == 17) {
-        i = 0;
+      if (counter == 17) {
+        counter = 0;
         return tuesdayString.elementAt(17);
       } else {
-        return tuesdayString.elementAt(i++);
+        return tuesdayString.elementAt(counter++);
       }
     } else {
       return "";
     }
   } else if (day == "Wednesday") {
     if ((wednesdayString != null) && (wednesdayString.isNotEmpty)) {
-      if (i == 17) {
-        i = 0;
+      if (counter == 17) {
+        counter = 0;
         return wednesdayString.elementAt(17);
       } else {
-        return wednesdayString.elementAt(i++);
+        return wednesdayString.elementAt(counter++);
       }
     } else {
       return "";
     }
   } else if (day == "Thursday") {
     if ((thursdayString != null) && (thursdayString.isNotEmpty)) {
-      if (i == 17) {
-        i = 0;
+      if (counter == 17) {
+        counter = 0;
         return thursdayString.elementAt(17);
       } else {
-        return thursdayString.elementAt(i++);
+        return thursdayString.elementAt(counter++);
       }
     } else {
       return "";
     }
   } else if (day == "Friday") {
     if ((fridayString != null) && (fridayString.isNotEmpty)) {
-      if (i == 17) {
-        i = 0;
+      if (counter == 17) {
+        counter = 0;
         return fridayString.elementAt(17);
       } else {
-        return fridayString.elementAt(i++);
+        return fridayString.elementAt(counter++);
       }
     } else {
       return "";
@@ -94,13 +100,17 @@ getValue(String day) {
     return "";
 }
 
+/* buildRow Widget that returns a Row Widget containing a Text, TextFormField, and TextFormField, each wrapped in Expanded to size appropriately.
+** Constructed with a String for the day, var for the corresponding array for that day, and String for the time of day.
+** The initial value is set to the value returned by getValue.
+** When the key for that day is saved through submitButton, all TextFormFields for that day have their values added to that day's array. 
+*/
 Widget buildRow(String day, var dayNames, String time) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: <Widget>[
-      //Time
-      Expanded(child: Text("Time: " + time)),
-      //Name
+      Expanded(
+        child: Text("Time: " + time)),
       Expanded(
           child: TextFormField(
         initialValue: getValue(day),
@@ -109,7 +119,6 @@ Widget buildRow(String day, var dayNames, String time) {
           dayNames.add(value);
         },
       )),
-      //Name
       Expanded(
           child: TextFormField(
         initialValue: getValue(day),
@@ -122,6 +131,10 @@ Widget buildRow(String day, var dayNames, String time) {
   );
 }
 
+/* submitButton Widget that returns an ElevatedButton that saves the data for that day.
+** Constructed with a var for the day, String for the name of the day, key, and context.
+** 'dayName' determines which 'day'array the data will be saved into.
+*/
 Widget submitButton(
     var day, String dayName, GlobalKey<FormState> key, BuildContext context) {
   return ElevatedButton(
@@ -133,93 +146,38 @@ Widget submitButton(
             Map<String, String> data = {
               "mondayNames": mondayNames.toString(),
             };
-            var response = await networkHandler.patch(
-                "/timetable/updatetableMonday/60b80f6cffdeeeb96a3fb0fe", data);
-            if (response.statusCode == 200 || response.statusCode == 201) {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => TeacherHome()),
-              // );
-              print("success");
-            } else {
-              print("error");
-            }
             day.clear();
           } else if (dayName == "Tuesday") {
             Map<String, String> data = {
               "tuesdayNames": tuesdayNames.toString(),
             };
-            var response = await networkHandler.patch(
-                "/timetable/updatetableTuesday/60b80f6cffdeeeb96a3fb0fe", data);
-            if (response.statusCode == 200 || response.statusCode == 201) {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => TeacherHome()),
-              // );
-              print("success");
-            } else {
-              print("error");
-            }
             day.clear();
           } else if (dayName == "Wednesday") {
             Map<String, String> data = {
               "wednesdayNames": wednesdayNames.toString(),
             };
-            var response = await networkHandler.patch(
-                "/timetable/updatetableWednesday/60b80f6cffdeeeb96a3fb0fe",
-                data);
-            if (response.statusCode == 200 || response.statusCode == 201) {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => TeacherHome()),
-              // );
-              print("success");
-            } else {
-              print("error");
-            }
             day.clear();
           } else if (dayName == "Thursday") {
             Map<String, String> data = {
               "thursdayNames": thursdayNames.toString(),
             };
-            var response = await networkHandler.patch(
-                "/timetable/updatetableThursday/60b80f6cffdeeeb96a3fb0fe",
-                data);
-            if (response.statusCode == 200 || response.statusCode == 201) {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => TeacherHome()),
-              // );
-              print("success");
-            } else {
-              print("error");
-            }
             day.clear();
           } else if (dayName == "Friday") {
             Map<String, String> data = {
               "fridayNames": fridayNames.toString(),
             };
-            var response = await networkHandler.patch(
-                "/timetable/updatetableFriday/60b80f6cffdeeeb96a3fb0fe", data);
-            if (response.statusCode == 200 || response.statusCode == 201) {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => TeacherHome()),
-              // );
-              print("success");
-            } else {
-              print("error");
-            }
+            day.clear();
           }
-          day.clear();
         }
-
-        print("Submit button pressed!");
-        print(dayName + ": " + day.toString());
-        print(mondayNames.toString());
       });
 }
 
+/* buildDay Function that returns a Form widget containing a buildRow for each hour from 7am til 3pm.
+** Constructed with a String for the day, var for the day's array, the key for that day, and the build context.
+** Each buildRow gets passed the day, and the array for that day, as well as the time to show as a String value.
+** A submitButton is added after all the buildRows to provide a method of data saving.
+** The submitButton is passed the day's array, name as String, the day's key, and the build context.
+*/
 Widget buildDay(
   String day,
   var dayNames,
@@ -245,60 +203,55 @@ Widget buildDay(
       ));
 }
 
+/* loadTimetable function that loads an existing timetable from the database into each day's arrays.
+** Constructed with a List<TimeModel> which represents the database data, and a String for the day.
+** As the data is stored in a '[, , ...]' format, the function first performs a substring to remove the '[' and ']'.
+** It then performs a split on the remaning String, which is saved into the day's array.
+*/
 loadTimetable(List<TimeModel> data, String day) async {
   newString = [];
   data.forEach((element) {
     if (day == 'Monday') {
       String mondayFull = element.mondayTable;
       if (mondayFull != null) {
-        print("before substr");
-        if ((mondayFull == null)) {
-          print("matched");
-        }
         mondayFull = mondayFull.substring(1, (mondayFull.length - 1));
         mondayString = mondayFull.split(", ");
-        print("LT/mondaystring: " + mondayString.toString());
         return mondayString;
       } else {
-        print("Else");
         mondayString = [];
       }
     } else if (day == 'Tuesday') {
       String tuesdayFull = element.tuesdayTable;
-      if (tuesdayFull != "" && tuesdayFull != null) {
+      if (tuesdayFull != null) {
         tuesdayFull = tuesdayFull.substring(1, (tuesdayFull.length - 1));
         tuesdayString = tuesdayFull.split(", ");
-        print("LT/tuestring: " + tuesdayString.toString());
         return tuesdayString;
       } else {
         tuesdayString = [];
       }
     } else if (day == 'Wednesday') {
       String wednesdayFull = element.wednesdayTable;
-      if (wednesdayFull != "" && wednesdayFull != null) {
+      if (wednesdayFull != null) {
         wednesdayFull = wednesdayFull.substring(1, (wednesdayFull.length - 1));
         wednesdayString = wednesdayFull.split(", ");
-        print("LT/wednesdaystring: " + wednesdayString.toString());
         return wednesdayString;
       } else {
         wednesdayString = [];
       }
     } else if (day == 'Thursday') {
       String thursdayFull = element.thursdayTable;
-      if (thursdayFull != "" && thursdayFull != null) {
+      if (thursdayFull != null) {
         thursdayFull = thursdayFull.substring(1, (thursdayFull.length - 1));
         thursdayString = thursdayFull.split(", ");
-        print("LT/thursdaystring: " + thursdayString.toString());
         return thursdayString;
       } else {
         thursdayString = [];
       }
     } else if (day == 'Friday') {
       String fridayFull = element.fridayTable;
-      if (fridayFull != "" && fridayFull != null) {
+      if (fridayFull != null) {
         fridayFull = fridayFull.substring(1, (fridayFull.length - 1));
         fridayString = fridayFull.split(", ");
-        print("LT/fridaystring: " + fridayString.toString());
         return fridayString;
       } else {
         fridayString = [];
@@ -317,14 +270,10 @@ class _TimetableState extends State<Timetable> {
   void initState() {
     super.initState();
     fetchData();
-    // ignore: todo
-    // TODO: implement initState
-    print("init");
-    setState(() {
-      print("reloaded");
-    });
+    setState(() {});
   }
 
+  //Fetches the data from the database and calls loadTimetable to load the data into each day.
   void fetchData() async {
     var response = await networkHandler.get("/timetable/getalltable-data");
     tableModel = TableModel.fromJson(response);
@@ -338,6 +287,7 @@ class _TimetableState extends State<Timetable> {
     });
   }
 
+  //Builds each day of the week onto their own PageView 'page'.
   @override
   Widget build(BuildContext context) {
     PageController controller = PageController(initialPage: 0);
